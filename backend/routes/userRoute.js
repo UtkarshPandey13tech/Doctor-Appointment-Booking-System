26097f472fffb927020535e14b89d9606992d21c
Utkarsh Verma <utkarshverma2092001@gmail.com>
2026-07-28 13:46:53 +0530

    Payment Integration

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { registerUser, loginUser, getProfile, updateProfile, bookAppointment } from '../controllers/userController.js'
+import { registerUser, loginUser, getProfile, updateProfile, bookAppointment, listAppointment, cancelAppointment, paymentRazorpay, verifyRazorpay } from '../controllers/userController.js'
 import authUser from '../middleware/authUser.js'
 import upload from '../middleware/multer.js'
 
@@ -11,7 +11,10 @@ userRouter.post('/login', loginUser)
 userRouter.get('/get-profile', authUser, getProfile)
 userRouter.post('/update-profile', authUser, upload.single('image'), updateProfile)
 userRouter.post('/book-appointment', authUser, bookAppointment)
-
+userRouter.get('/appointments', authUser, listAppointment)
+userRouter.post('/cancel-appointment', authUser, cancelAppointment)
+userRouter.post('/payment-razorpay', authUser, paymentRazorpay)
+userRouter.post('/verify-razorpay', authUser, verifyRazorpay)
 
 
 
